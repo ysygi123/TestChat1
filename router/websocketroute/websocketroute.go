ac01beec74c083af31776e65dbad649787e546d5
ysygi123 <1179364733@qq.com>
2021-06-18 21:40:59 +0800

    添加路由

--- router/websocketroute/websocketroute.go
+++ router/websocketroute/websocketroute.go
@@ -1,3 +1,13 @@
 package websocketroute
 
+import (
+	"TestChat1/controller/websocketcontroller"
+	"net/http"
+)
+
 type websocketFunc func()
+
+func StartWebSocketRoute() {
+	http.HandleFunc("/ws", websocketcontroller.FirstPage)
+	http.ListenAndServe(":8087", nil)
+}
