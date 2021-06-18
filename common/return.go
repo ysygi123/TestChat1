c9ada56a3b7602c17fa4904b48c58143dc57f793
ysygi123 <1179364733@qq.com>
2021-06-18 21:40:14 +0800

    将response改为公用

--- common/return.go
+++ common/return.go
@@ -5,16 +5,16 @@ import (
 	"net/http"
 )
 
-type response struct {
-	Code int `json:"code"`
-	Message string `json:"message"`
-	Data interface{} `json:"data"`
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
 
-func ReturnResponse(c *gin.Context, code int, statusCode int, message string, data interface{})  {
-	c.JSON(http.StatusOK, response{
-		Code:statusCode,
-		Message:message,
-		Data:data,
+func ReturnResponse(c *gin.Context, code int, statusCode int, message string, data interface{}) {
+	c.JSON(http.StatusOK, Response{
+		Code:    statusCode,
+		Message: message,
+		Data:    data,
 	})
 }
