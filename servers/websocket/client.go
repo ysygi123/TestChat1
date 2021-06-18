c882832a6598bc69c75706df4261be1a704340c6
ysygi123 <1179364733@qq.com>
2021-06-18 21:42:50 +0800

    添加两个字段

--- servers/websocket/client.go
+++ servers/websocket/client.go
@@ -1,15 +1,21 @@
 package websocket
 
+import "github.com/gorilla/websocket"
+
 type client struct {
-	Ip string
-	Id int
-	HeartBreath uint64
+	Ip             string
+	Id             int
+	HeartBreath    uint64
+	BelongServerID int
+	WebSocketConn  websocket.Conn
 }
 
-func NewClient(ip string, id int, heartBreath uint64) *client {
+func NewClient(ip string, id int, heartBreath uint64, websocketconn websocket.Conn) *client {
 	return &client{
-		Ip:ip,
-		Id:id,
-		HeartBreath:heartBreath,
+		Ip:             ip,
+		Id:             id,
+		HeartBreath:    heartBreath,
+		BelongServerID: 1,
+		WebSocketConn:  websocketconn,
 	}
 }
