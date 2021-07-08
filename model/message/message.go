d5f17d040ea24f4c8c7a4e50c7394a2f8a6df188
ysygi123 <1179364733@qq.com>
2021-07-08 23:03:05 +0800

    消息列表结构体调整

--- model/message/message.go
+++ model/message/message.go
@@ -19,13 +19,13 @@ type Message struct {
 
 type MessageList struct {
 	MessageType    uint8  `db:"message_type" json:"message_type"`
-	MessageNum     uint8  `db:"message_num" json:"message_num"`
 	IsDel          uint8  `db:"is_del" json:"is_del"`
+	MessageNum     int    `db:"message_num" json:"message_num"`
 	Id             int    `db:"id" json:"id"`
 	Uid            int    `db:"uid" json:"uid"`
 	FromId         int    `db:"from_id" json:"from_id"`
 	CreatedTime    uint64 `db:"created_time" json:"created_time"`
-	UpdateTime     uint64 `db:"created_time" json:"created_time"`
+	UpdateTime     uint64 `db:"update_time" json:"update_time"`
 	MessageContent string `db:"message_content" json:"message_content"`
 	MessageId      int    `db:"message_id" json:"message_id"`
 }
